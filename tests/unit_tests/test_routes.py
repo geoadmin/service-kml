479da1bd0a4585dadb85c3756818fecbbebaef22
Juergen Hansmann <Juergen.Hansmann@swisstopo.ch>
2021-06-30 22:52:22 +0200

    BGDIINF_SB-1799 raise error in unit tests
    in case the bucket and the table that are mocked in the unit test already exist, an error is raised.
    As the bucket and the table are deleted in the tearDown(), there should not be any existing bucket
    and table in setUp(). This also makes sure that we do not accidentally use real resources.

--- tests/unit_tests/test_routes.py
+++ tests/unit_tests/test_routes.py
@@ -1,3 +1,4 @@
+import logging
 import unittest
 
 import boto3
@@ -9,6 +10,8 @@ from app.settings import AWS_DB_TABLE_NAME
 from app.settings import AWS_S3_BUCKET_NAME
 from app.settings import AWS_S3_REGION_NAME
 from app.version import APP_VERSION
+
+logger = logging.getLogger(__name__)
 
 
 class CheckerTests(unittest.TestCase):
@@ -40,25 +43,36 @@ class TestPostEndpoint(unittest.TestCase):
         <py:elem1 />
         <elem2 xmlns="" />
         </root>"""
-        s3bucket = boto3.resource('s3', region_name=AWS_S3_REGION_NAME)
-        location = {'LocationConstraint': AWS_S3_REGION_NAME}
-        s3bucket.create_bucket(Bucket=AWS_S3_BUCKET_NAME, CreateBucketConfiguration=location)
-        dynamodb = boto3.resource(
-            'dynamodb', region_name=AWS_DB_REGION_NAME, endpoint_url=AWS_DB_ENDPOINT_URL
-        )
-        dynamodb.create_table(
-            TableName=AWS_DB_TABLE_NAME,
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'adminId', 'AttributeType': 'S'
-                },
-            ],
-            KeySchema=[
-                {
-                    'AttributeName': 'adminId', 'KeyType': 'HASH'
-                },
-            ]
-        )
+        try:
+            s3bucket = boto3.resource('s3', region_name=AWS_S3_REGION_NAME)
+            location = {'LocationConstraint': AWS_S3_REGION_NAME}
+            s3bucket.create_bucket(Bucket=AWS_S3_BUCKET_NAME, CreateBucketConfiguration=location)
+        except s3bucket.meta.client.exceptions.BucketAlreadyExists as err:
+            logger.debug(
+                "Bucket %s already exists but should not.", err.response['Error']['BucketName']
+            )
+            raise err
+
+        try:
+            dynamodb = boto3.resource(
+                'dynamodb', region_name=AWS_DB_REGION_NAME, endpoint_url=AWS_DB_ENDPOINT_URL
+            )
+            dynamodb.create_table(
+                TableName=AWS_DB_TABLE_NAME,
+                AttributeDefinitions=[
+                    {
+                        'AttributeName': 'adminId', 'AttributeType': 'S'
+                    },
+                ],
+                KeySchema=[
+                    {
+                        'AttributeName': 'adminId', 'KeyType': 'HASH'
+                    },
+                ]
+            )
+        except dynamodb.meta.client.exceptions.ResourceInUseException as err:
+            logger.debug("Table %s already exists but should not.", AWS_DB_TABLE_NAME)
+            raise err
 
     def tearDown(self):
         s3bucket = boto3.resource('s3', region_name=AWS_S3_REGION_NAME).Bucket(AWS_S3_BUCKET_NAME)
